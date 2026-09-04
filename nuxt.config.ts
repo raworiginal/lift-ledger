59fce9ed0ad8c00b471b64eed9f1c115344d98d0
Randall Wolfe <randall.a.wolfe@gmail.com>
2026-09-03 18:05:38 -0700

    installed tailwind & daisy ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/better-auth", "@nuxthub/core"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   auth: {
     redirects: {
       login: "/login",
