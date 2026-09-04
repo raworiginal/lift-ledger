2a0e25328b1c86c6051d97a387e2ea3d69091a24
raworiginal <118763950+raworiginal@users.noreply.github.com>
2026-09-04 09:54:54 -0700

    moved LoginForm to its own component

--- app/components/LandingPage.vue
+++ app/components/LandingPage.vue
@@ -3,16 +3,9 @@
     <div class="hero-content text-center">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Lift Ledger</h1>
-        <fieldset
-          class="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4"
-        >
-          <legend class="fieldset-legend">Login</legend>
-          <label class="label">Email</label>
-          <input type="email" class="input" placeholder="Email" />
-          <label class="label">Password</label>
-          <input type="password" class="input" placeholder="Password" />
-          <button class="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        <div role="tablist" class="tabs tabs-lift">
+          <LoginForm />
+        </div>
       </div>
     </div>
   </div>
